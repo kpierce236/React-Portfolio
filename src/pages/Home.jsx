dead85ce49866c8aa902859a8872e5ecfe2042c9
Kevin Pierce <kpierce236@gmail.com>
2024-03-23 23:18:48 -0400

    added home page content and style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,22 @@
 import React from 'react';
 
 function Home() {
-  return <h2>Home Page</h2>;
+  return (
+  <div className="container">
+  <div className="row align-items-center">
+    <div className="col-md-6">
+      <div className="home-content">
+        <h1>Welcome to My Portfolio</h1>
+        <p>Discover my projects, learn more about me, and get in touch!</p>
+      </div>
+    </div>
+    <div className="col-md-6">
+      <div className="home-image">
+        <img src="https://via.placeholder.com/400" alt="Developer" className="img-fluid rounded" />
+      </div>
+    </div>
+  </div>
+</div>)
 }
 
 export default Home;
