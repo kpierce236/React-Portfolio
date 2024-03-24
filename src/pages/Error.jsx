1ff0bdcf28eb3b4b40b6cdca3fbf123530fb4e72
Kevin Pierce <kpierce236@gmail.com>
2024-03-23 23:16:22 -0400

    added style and error status and error message to error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
 import { useRouteError } from 'react-router-dom';
 
-
-function ErrorPage() {
+export default function ErrorPage() {
   const error = useRouteError();
   console.error(error);
 
+    // Extract relevant error information
+    const { status, statusText, data } = error;
+
+    const errorMessage = data;
+    // Prepare error message
+    const errorStatus = `${status}: ${statusText}`;
+
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
+    <div className="container text-center mt-5">
+      <h1 className="display-4">Oops!</h1>
+      <p className="lead">Sorry, an unexpected error has occurred.</p>
+      <p className="text-danger">
+        <i> <span className="text-danger">{errorStatus}</span></i><br />
+        <i> <span className="text-warning">{errorMessage}</span></i>
       </p>
     </div>
   );
 }
-
-export default ErrorPage;
