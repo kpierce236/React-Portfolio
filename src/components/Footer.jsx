2800f1162b9de9bee270b6f3e661ca1093979605
Kevin Pierce <kpierce236@gmail.com>
2024-03-23 21:37:35 -0400

    added bootstrap and styling to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,20 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>
-      <ul className="social-icons">
-        <li><a href="https://github.com/kpierce236"><i className="fab fa-github"></i></a></li>
-        <li><a href="https://www.linkedin.com/in/kevin-pierce-70903410a/"><i className="fab fa-linkedin"></i></a></li>
-        <li><a href="link-to-other-profile"><i className="fas fa-user"></i></a></li>
-      </ul>
+    <footer className="bg-light text-center">
+      <div className="container p-4">
+        <ul className="list-inline">
+          <li className="list-inline-item"><a href="link-to-github-profile"><i className="fab fa-github fa-2x"></i></a></li>
+          <li className="list-inline-item"><a href="link-to-linkedin-profile"><i className="fab fa-linkedin fa-2x"></i></a></li>
+          <li className="list-inline-item"><a href="link-to-other-profile"><i className="fas fa-user fa-2x"></i></a></li>
+        </ul>
+      </div>
+      <div className="text-center p-3 bg-dark text-white">
+        &copy; 2024 Your Company
+      </div>
     </footer>
   );
 }
 
 export default Footer;
+
