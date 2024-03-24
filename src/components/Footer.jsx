e74f43df2cb190b9530d9aaeae33b9e3331d0bab
Kevin Pierce <kpierce236@gmail.com>
2024-03-23 21:05:32 -0400

    added Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+// Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <ul className="social-icons">
+        <li><a href="https://github.com/kpierce236"><i className="fab fa-github"></i></a></li>
+        <li><a href="https://www.linkedin.com/in/kevin-pierce-70903410a/"><i className="fab fa-linkedin"></i></a></li>
+        <li><a href="link-to-other-profile"><i className="fas fa-user"></i></a></li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
